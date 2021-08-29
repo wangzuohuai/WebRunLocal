91d45bb91e3b81126fad605dded0c91ff97eb150
wangzuohuai <Huaizuo007>
2021-08-29 17:07:01 +0800

    网络版2.2.1.1(单机版1.5.1.1) 重要版本-强烈推荐更新     1、中间件支持普通用户使用，不再需要管理员权限，大幅改善用户体验和系统安全性能；     2、运行数据统一保存到中间件Data子目录，解决切换桌面用户时需重新安装问题；     3、高级版内嵌小程序增加支持在扩展屏上兼容运行；     4、安装时exe小程序自动增加防火墙例外设置，避免运行时提示；     4、请求版本协议中增加获取本机和路由器IP功能；     5、Office网页内嵌小程序增加书签相关功能，支持文件套红操作；     6、Office网页内嵌小程序支持打开和插入远程文件功能；     7、解决内嵌小程序启动可能无响应或提示超时问题；     8、解决内嵌小程序启动时切换了窗口再切回时窗口错位问题；     9、解决内嵌小程序启动时前端可能收不到启动成功的通知问题；     10、解决网络版部分注册表错误问题，已开发的C#小程序需要重新编译发布

--- Bin/Plugins/E7C7BDA6-C828-46F1-A7BA-B4C572A01100/Reg.bat
+++
@@ -1,6 +0,0 @@
-IF NOT EXIST "%ProgramW6432%" (
-regsvr32 /s "%WinDir%\system32\Macromed\Flash\Flash.ocx"
-) ELSE (
-regsvr32 /s "%WinDir%\SysWOW64\Macromed\Flash\Flash.ocx"
-regsvr32 /s "%WinDir%\system32\Macromed\Flash\Flash.ocx"
-)
