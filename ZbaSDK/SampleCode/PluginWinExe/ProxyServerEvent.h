19df715100b76e0cb1d8b69a702821f10ccfbe08
zorrosoft <wzh@zorrosoft.com>
2023-02-08 17:00:23 +0800

    网络版2.2.9.1(单机版1.5.9.1)    1、优化Windows 8及以上版本水印版文字显示效果及对小程序窗口做透明或半透明的效果；    2、优化中间件及网络版的授权服务自动恢复逻辑，另外改为非延迟自动启动，避免前端请求时服务还未就绪问题；    3、网络版授权服务器接入新网卡不再影响使用，对终端电脑增加审核转自动和自动转审核的授权模式，并可对每个电脑实现单独配置，解决系统睡眠恢复提示错误问题，解决侦听异常时可能不能正常恢复服务问题；    4、海康及多引擎网页播放器支持叠加指定网页内容，支持透明或半透明叠加显示；    5、海康及多引擎网页播放器优化Windows 8及以上版本状态文字显示效果，解决可能不消失问题，加大播放失败重试时间间隔，解决充满窗口播放设置可能不生效问题；    6、微软Office及金山WPS网页组件完整嵌入时去掉打开历史记录，优化密码打开方式支持

--- ZbaSDK/SampleCode/PluginWinExe/ProxyServerEvent.h
+++ ZbaSDK/SampleCode/PluginWinExe/ProxyServerEvent.h
@@ -6,63 +6,105 @@
 #ifndef WM_APPLET_CLOSE
 #define WM_APPLET_CLOSE			WM_USER + 3000
 #endif
+
 /// 通知小程序初始化完成
 #ifndef WM_APPLET_INITOK
 #define WM_APPLET_INITOK		WM_USER + 3001
 #endif
-/// 通知小程序重绘
+
+/// 通知小程序窗口重绘
 #ifndef WM_APPLET_REDRAW
 #define WM_APPLET_REDRAW		WM_USER + 3002
 #endif
-/// 通知小程序显示还是隐藏
+
+/// 通知小程序窗口显示还是隐藏
 #ifndef WM_APPLET_SHOW
 #define WM_APPLET_SHOW			WM_USER + 3003
 #endif
+
 /// 通知小程序状态(比如播放暂停，如果在播放时)
 #ifndef WM_APPLET_STATUS
 #define WM_APPLET_STATUS		WM_USER + 3004
 #endif
-/// 全屏状态通知
+
+/// 通知小程序窗口全屏状态
 #ifndef WM_APPLET_FULLSCREEN
 #define WM_APPLET_FULLSCREEN	WM_USER + 3005
 #endif
-/// 通知点击
+
+/// 通知点击小程序窗口
 #ifndef WM_APPLET_CLICKDOWN
 #define WM_APPLET_CLICKDOWN		WM_USER + 3006
 #endif
-/// 通知移动
+
+/// 通知移动小程序窗口
 #ifndef WM_APPLET_MOVEWND
 #define WM_APPLET_MOVEWND		WM_USER + 3007
 #endif
-/// 通知设置焦点
+
+/// 通知小程序窗口设置焦点
 #ifndef WM_APPLET_SETAPPLETFOCUS
 #define WM_APPLET_SETAPPLETFOCUS WM_USER + 3013
 #endif
 
+/// 设置小程序窗口内容保护
+#ifndef WM_APPLET_PROTECTWND
+#define WM_APPLET_PROTECTWND	WM_USER + 3020
+#endif
+
+/// 设置小程序窗口内容保护
+#ifndef WM_APPLET_ACTIVEWND
+#define WM_APPLET_ACTIVEWND		WM_USER + 3021
+#endif
+
+/// 设置嵌入的第三方进程窗口
+#ifndef WM_APPLET_OTHERWND
+#define WM_APPLET_OTHERWND		WM_USER + 3022
+#endif
+
+/// 通知保存文件
 #ifndef WM_APPLET_SAVEFILE
 #define WM_APPLET_SAVEFILE		WM_USER + 3050
 #endif
+
+/// 通知关闭文档
 #ifndef WM_APPLET_CLOSEDOC
 #define WM_APPLET_CLOSEDOC		WM_USER + 3051
 #endif
+
+/// 通知文档新建或打开
 #ifndef WM_APPLET_DOCNEWOROPEN
 #define WM_APPLET_DOCNEWOROPEN	WM_USER + 3052
 #endif
+#ifndef WM_APPLET_NETFILE
+#define WM_APPLET_NETFILE		WM_USER + 3053
+#endif
 
+/// 通知初始化打开
 #ifndef WM_APP_INITOPEN
-#define WM_APP_INITOPEN		WM_USER + 500
-#endif
-#ifndef WM_APP_OTHERPID
-#define WM_APP_OTHERPID		WM_USER + 501
+#define WM_APP_INITOPEN			WM_USER + 500
 #endif
 
+/// 通知其它进程ID
+#ifndef WM_APP_OTHERPID
+#define WM_APP_OTHERPID			WM_USER + 501
+#endif
+
+/// WS新连接通知
 #define WM_PROXYSEREREVENT_NEWCONNECT	WM_USER + 2000
+/// WS收到JSON包
 #define WM_PROXYSEREREVENT_RECMESSAGE	WM_USER + 2001
+/// WS收到文字包
 #define WM_PROXYSEREREVENT_RECTEXT		WM_USER + 2002
+/// WS收到二进制流包
 #define WM_PROXYSEREREVENT_BYTE			WM_USER + 2003
+/// WS收到关闭通知
 #define WM_PROXYSEREREVENT_CLOSECONN	WM_USER + 2005
+/// WS收到HTTP同步请求通知
 #define WM_PROXYSEREREVENT_HTTPREQ		WM_USER + 2006
+/// WS收到HTTP同步端口通知
 #define WM_PROXYSEREREVENT_HTTPPORT		WM_USER + 2007
+/// WS收到错误通知
 #define WM_PROXYSEREREVENT_RECERR		WM_USER + 2008
 
 #ifndef NEED_FINAL_CONSTRUCT
